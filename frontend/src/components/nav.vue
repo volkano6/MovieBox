90b27b8be35bd7be54c1892ecd1a6af9c9508c5c
ceirem <72570708+ceirem@users.noreply.github.com>
2022-12-07 01:32:23 +0300

    Navbar is updated

--- frontend/src/components/nav.vue
+++ frontend/src/components/nav.vue
@@ -1,42 +1,55 @@
 <template>
-  <div class="container-sm ">
-    <nav class="navbar navbar-expand-lg bg-light">
+<section class= nav>
+  <div class="container-sm border-bottom-dark">
+    <nav class="navbar navbar-expand-lg bg-gray">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+      <a class="navbar-brand" href="#"><img id="logo"
+        src="https://a.ltrbxd.com/logos/letterboxd-mac-icon.png" alt="MDB Logo"
+        draggable="false" height="40" /></a>
+    <a class="navbar-brand text-light" href="#">MovieBoxd</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="col-4">
+        <div class="col-md-5">
+          <form class="d-flex input-group w-auto my-auto mb-3 mb-md-0">
+            <input autocomplete="off" type="search" class="form-control rounded" placeholder="Search" />
+          </form>
+        </div>
+        </div>
+       <div class="col-8">
+         <div class="col-md-9">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/home">Home</a>
+          <a class="nav-link active text-light text-opacity-75" aria-current="page" href="/home">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/profile">Profile</a>
+          <a class="nav-link text-light text-opacity-75" href="/profile">Profile</a>
         </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
+         <li class="nav-item">
+          <a class="nav-link text-light text-opacity-75" href="/#">Films</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+          <a class="nav-link d-flex align-items-center text-light text-opacity-75" href="/login">Sign In</a>
         </li>
+          <li class="nav-item">
+          <a class="nav-link d-flex align-items-center text-light text-opacity-75" href="/login"> Log Out</a>
+        </li>
+
       </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
+       </ul>
+      </div>
+      </div>
+  
+
     </div>
   </div>
 </nav>
   </div>
+
+  </section>
 </template>
 
 <script>
@@ -46,5 +59,11 @@ export default {
 </script>
 
 <style>
+
+.nav {
+
+background-image: radial-gradient(circle,  #6e799e,  #304b51, #273738, #1e2423)
+}
+
 
 </style>
--- frontend/src/views/profile.vue
+++ frontend/src/views/profile.vue
@@ -1,79 +1,83 @@
 <template>
   <div>
     <Nav></Nav>
-    <div class="container-sm mt-3">
-      <div class="row">
-        <div class="col-4">
-          <div>
-            <div class="">
-              <div class="user text-center">
-                <div class="profile mt-3">
-                  <img src="https://i.imgur.com/JgYD2nQ.jpg" class="rounded-circle" width="90">
+    <section class= "profile1">
+      <div class="container-sm">
+        <div class="row">
+          <div class="col-4">
+            <div>
+              <div class="">
+                <div class="user text-center">
+                  <div class="profile mt-3">
+                    <img src="https://i.imgur.com/JgYD2nQ.jpg" class="rounded-circle" width="90">
+                  </div>
+                </div>
+                <div class="text-center">
+                  <h4 class="m-3 text-light text-opacity-75" >User</h4>
                 </div>
               </div>
-              <div class="text-center">
-                <h4 class="m-3">Volkan</h4>
+            </div>
+          </div>
+          <div class="col-8 mt-5">
+            <div class="row justify-content-start">
+              <div class="col-2">
+                <div class="row text-center">
+                  <h4 class="text-light text-opacity-75">70</h4>
+                  <p class="text-light text-opacity-75">Watched</p>
+                </div>
+              </div>
+              <div class="col-2">
+                <div class="row text-center">
+                  <h4 class="text-light text-opacity-75">45</h4>
+                  <p class="text-light text-opacity-75">Watchlist</p>
+                </div>
+              </div>
+              <div class="col-2">
+                <div class="row text-center">
+                  <h4 class="text-light text-opacity-75">360</h4>
+                  <p class="text-light text-opacity-75">Comments</p>
+                </div>
+              </div>
+              <div class="col-2">
+                <div class="row text-center">
+                   <h4 class="text-light text-opacity-75">450</h4>
+                  <p class="text-light text-opacity-75">Likes</p>
+                </div>
               </div>
             </div>
           </div>
         </div>
-        <div class="col-8 mt-5">
-          <div class="row justify-content-start">
-            <div class="col-2">
-              <div class="row text-center">
-                <h4>456</h4>
-                <p class="text-secondary text-opacity-75">Watched</p>
-              </div>
-            </div>
-            <div class="col-2">
-              <div class="row text-center">
-                <h4>456</h4>
-                <p class="text-secondary text-opacity-75">Watchlist</p>
-              </div>
-            </div>
-            <div class="col-2">
-              <div class="row text-center">
-                <h4>456</h4>
-                <p class="text-secondary text-opacity-75">Comments</p>
-              </div>
-            </div>
-            <div class="col-2">
-              <div class="row text-center">
-                <h4>569</h4>
-                <p class="text-secondary text-opacity-75">Likes</p>
-              </div>
-            </div>
+        <div class="row">
+          
+          <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
+            <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off" checked>
+            <label class="btn btn-light" for="btnradio1">Profile</label>
+
+            <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off">
+            <label class="btn btn-light" for="btnradio2" >Watched</label>
+
+            <input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off">
+            <label class="btn btn-light" for="btnradio3">Watchlist</label>
+
+            <input type="radio" class="btn-check" name="btnradio" id="btnradio4" autocomplete="off">
+            <label class="btn btn-light" for="btnradio4">Comments</label>
+
+            <input type="radio" class="btn-check" name="btnradio" id="btnradio5" autocomplete="off">
+            <label class="btn btn-light btn-link-color: #ff" for="btnradio5">Likes</label>
           </div>
         </div>
-      </div>
-      <div class="row">
-        <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
-          <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off" checked>
-          <label class="btn btn-outline-dark" for="btnradio1">Profile</label>
-
-          <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off">
-          <label class="btn btn-outline-dark" for="btnradio2">Watched</label>
-
-          <input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off">
-          <label class="btn btn-outline-dark" for="btnradio3">Watchlist</label>
-
-          <input type="radio" class="btn-check" name="btnradio" id="btnradio4" autocomplete="off">
-          <label class="btn btn-outline-dark" for="btnradio4">Comments</label>
-
-          <input type="radio" class="btn-check" name="btnradio" id="btnradio5" autocomplete="off">
-          <label class="btn btn-outline-dark" for="btnradio5">Likes</label>
+        <div class="row mt-2 row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-3">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
         </div>
       </div>
-      <div class="row mt-2 row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-3">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-      </div>
+      <Footer></Footer>
+        </section>
     </div>
-    <Footer></Footer>
-  </div>
+
 </template>
   
 <script>
@@ -107,4 +111,11 @@ export default {
   
 <style>
 
+.profile1 {
+ background-image: radial-gradient(circle, #686e91, #626988, #5c647e, #575f75, #525a6b, #4a5462, #444d58, #3e474f, #343e43, #2c3638, #242d2d, #1e2423);
+}
+
+.row {
+  color: #626988;
+}
 </style>
