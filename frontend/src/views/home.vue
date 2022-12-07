29ea4eeb7f9d3412eec070eb23f192dba2b478d0
efeardaulun <efeardaulun@gmail.com>
2022-12-07 20:27:23 +0300

    Update home.vue

--- frontend/src/views/home.vue
+++ frontend/src/views/home.vue
@@ -1,18 +1,105 @@
 <template>
-  <div>
+  <div class="profile1">
     <Nav></Nav>
-    <div class="container">
+    <div class="container" >
+      <div class="text-center" style="padding-left:35%;padding-right:35%">
+      <h2 class="text-center" style="margin-top: 2%; color: aliceblue; border-bottom: 2px solid aliceblue; padding-bottom: 0.7%;"> Favorite of Weeks </h2> 
+    </div>
       <div class="row mt-2 row-cols-1 row-cols-xl-5 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-3">
         <Card />
         <Card />
         <Card />
         <Card />
         <Card />
-        <Card />
-        <Card />
-        <Card />
+
       </div>
+
     </div>
+
+    <div class="container" >
+      <div class="text-center" style="margin-top: 3%;padding-left:35%;padding-right:35%">
+      <h2 class="text-center" style="margin-top: 2%; color: aliceblue; border-bottom: 2px solid aliceblue; padding-bottom: 0.7%;"> All Time Favorites </h2> 
+    </div>
+      <div class="row mt-2 row-cols-1 row-cols-xl-5 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-3">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+
+      </div>
+
+    </div>
+      <div class="text-left" style="padding-left:7.7%;padding-right:7%">
+      <h3 class="text-left" style="margin-top: 5%; color: aliceblue; border-bottom: 2px solid aliceblue; padding-bottom: 0.7%;"> Last Reviews </h3> 
+      </div>
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+<div class="container bootstrap snippets bootdey" style="margin-top: 1.5%; padding-left: 1.7%;">
+    <div class="row">
+        <div class="post-list">
+            <div class="row">
+                <div class="col-sm-1">
+                    <div class="picture">
+                        <img alt="Opt wizard thumbnail" style="width: 70%; height: 70%; float: right;" src="https://bootdey.com/img/Content/user_1.jpg">
+                    </div>
+                </div>
+                <div class="col-sm-8">
+                    <h4>
+                        <a hre="#" class="username">Deyson Bejarano</a>
+                        <label class="label label-info" style="color:cornflowerblue">&nbsp; #Film Name </label>
+                    </h4>
+                    <h5> 
+                    <i class="fa fa-calendar"></i>
+                    Aug 06, 12:48 
+                    </h5>
+                    <p class="description" style="color: aliceblue;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In velit lectus, efficitur eu eros vel, luctus aliquet est. Sed sit amet ligula non mauris porta dignissim..</p>    
+                </div>
+            </div>
+        </div>
+        <div class="post-list">
+            <div class="row">
+                <div class="col-sm-1">
+                    <div class="picture">
+                        <img alt="Opt wizard thumbnail" style="width: 70%; height: 70%; float: right;" src="https://bootdey.com/img/Content/user_2.jpg">
+                    </div>
+                </div>
+                <div class="col-sm-8">
+                    <h4>
+                        <a hre="#" class="username">Starla muscle</a>
+                        <label class="label label-info" style="color:cornflowerblue">&nbsp; #Film Name </label>
+                    </h4>
+                    <h5> 
+                    <i class="fa fa-calendar"></i>
+                    Aug 06, 12:48 
+                    </h5>
+                    <p class="description" style="color: aliceblue;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In velit lectus, efficitur eu eros vel, luctus aliquet est. Sed sit amet ligula non mauris porta dignissim....</p>    
+                </div>
+                
+            </div>
+        </div>
+        <div class="post-list" >
+            <div class="row" style="border:0px">
+                <div class="col-sm-1">
+                    <div class="picture">
+                        <img alt="Opt wizard thumbnail" style="width: 70%; height: 70%; float: right;" src="https://bootdey.com/img/Content/user_3.jpg">
+                    </div>
+                </div>
+                <div class="col-sm-8">
+                    <h4>    
+                        <a hre="#" class="username">Barbao"s slim</a>
+                        <label class="label label-info" style="color:cornflowerblue">&nbsp; #Film Name </label>
+                    </h4>
+                    <h5> 
+                    <i class="fa fa-calendar"></i>
+                    Aug 06, 12:48 
+                    </h5>
+                    <p class="description" style="color: aliceblue;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In velit lectus, efficitur eu eros vel, luctus aliquet est. Sed sit amet ligula non mauris porta dignissim....</p>    
+                </div>
+                
+            </div>
+        </div>
+    </div>
+</div>
     <Footer></Footer>
   </div>
 </template>
@@ -47,5 +134,77 @@ export default {
 </script>
   
 <style>
+.profile1 {
+ background-image: radial-gradient(circle, #686e91, #626988, #5c647e, #575f75, #525a6b, #4a5462, #444d58, #3e474f, #343e43, #2c3638, #242d2d, #1e2423);
+}
 
+.post-list {
+  position: relative;
+  padding: 5px 0;
+}
+
+.post-list .picture {
+  max-width: 400px;
+  overflow: hidden;
+  height: auto;
+  border-radius: 6px;
+}
+
+.post-list .label{
+    font-weight:normal;    
+}
+
+.post-list .picture {
+  max-width: 210px;
+}
+
+.post-list .picture img {
+  width: 100%;
+}
+
+.post-list h4 {
+  font-size: 20px;
+}
+
+.post-list h5 {
+  color: #888;
+}
+
+.post-list p {
+  float: left;
+}
+
+.post-list:after {
+  background: #EEEEEE;
+  width: 83.3333%;
+  bottom: 0;
+  right: 0;
+  content: "";
+  display: block;
+  position: absolute;
+}
+
+.post-list .btn-download {
+  margin-top: 45px;
+}
+
+.btn-info {
+  color: #1084FF;
+  border-color: #269abc;
+}
+
+.btn-round {
+  border-width: 1px;
+  border-radius: 30px !important;
+  opacity: 0.79;
+  padding: 9px 18px;
+}
+.btn {
+  border-width: 2px;
+  background-color: rgba(0,0,0,0);
+  font-weight: 400;
+  opacity: 0.8;
+  padding: 7px 16px;
+}
 </style>
+
