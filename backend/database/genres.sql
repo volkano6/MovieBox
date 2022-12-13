54c42417a7441fe4726e8a0c1191248fc4febd02
Arda Serdar Pektezol <1669855+pektezol@users.noreply.github.com>
2022-12-13 11:07:45 +0300

    sample movie genres done for 100 movies

---
+++ backend/database/genres.sql
@@ -0,0 +1,23 @@
+DELETE FROM genres;
+INSERT INTO genres (genre) VALUES
+('Action'),
+('Adventure'),
+('Animation'),
+('Biography'),
+('Comedy'),
+('Crime'),
+('Drama'),
+('Family'),
+('Fantasy'),
+('Film-Noir'),
+('History'),
+('Horror'),
+('Music'),
+('Musical'),
+('Mystery'),
+('Romance'),
+('Sci-Fi'),
+('Sport'),
+('Thriller'),
+('War'),
+('Western');
