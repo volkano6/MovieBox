c19014e8632afe77f2493e9496073cf56f6840d8
Volkan Öztoklu <volkanoztoklu@gmail.com>
2022-12-14 14:18:33 +0300

    number_of_likes changed with number_of_favorites

--- frontend/src/components/single_page/profile_page/user_favorites.vue
+++ frontend/src/components/single_page/profile_page/user_favorites.vue
@@ -2,7 +2,7 @@
   <div class="row mt-2 row-cols-1 row-cols-xl-5 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-3">
     <div v-for="movie in favorites" :key="movie.id">
       <MovieCard :poster=movie.poster :title=movie.title :date="movie.release_date" :genre=movie.genres
-        :rating="movie.rating" :length=movie.length :number_of_likes=movie.favorite_count
+        :rating="movie.rating" :length=movie.length :number_of_favorites=movie.favorite_count
         :number_of_watches=movie.watched_count />
     </div>
   </div>
--- frontend/src/views/home.vue
+++ frontend/src/views/home.vue
@@ -12,7 +12,7 @@
         <div class="row mt-2 row-cols-1 row-cols-xl-5 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-3">
           <div v-for="movie in movies.monthly_popular" :key="movie.id">
             <MovieCard :poster=movie.poster :title=movie.title :date="movie.release_date" :genre=movie.genres
-              :rating="movie.rating" :length=movie.length :number_of_likes=movie.favorite_count
+              :rating="movie.rating" :length=movie.length :number_of_favorites=movie.favorite_count
               :number_of_watches=movie.watched_count />
           </div>
         </div>
@@ -31,7 +31,7 @@
         <div class="row mt-2 row-cols-1 row-cols-xl-5 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-3">
           <div v-for="movie in movies.all_time_favorites" :key="movie.id">
             <MovieCard :poster=movie.poster :title=movie.title :date="movie.release_date" :genre=movie.genres
-              :rating="movie.rating" :length=movie.length :number_of_likes=movie.favorite_count
+              :rating="movie.rating" :length=movie.length :number_of_favorites=movie.favorite_count
               :number_of_watches=movie.watched_count />
           </div>
         </div>
