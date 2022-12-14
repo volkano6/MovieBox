e69bf8cb3eae462c5bd2f5677d23afb1492085bd
Arda Serdar Pektezol <1669855+pektezol@users.noreply.github.com>
2022-12-14 12:29:46 +0300

    fully working films backend done with genres, watched and favorite count

--- backend/controllers/movies.go
+++ backend/controllers/movies.go
@@ -2,29 +2,35 @@ package controllers
 
 import (
 	"net/http"
-	"fmt"
+
 	"github.com/C305DatabaseProject/database-project/backend/database"
 	"github.com/gin-gonic/gin"
 )
 
 func GetMovies(c *gin.Context) {
-	// Check comments with user id
-	sql := `SELECT id, title, release_date, poster, rating, length FROM movies;`
+	// Get all movie data from the database with genres, favorite and watched count
+	sql := `SELECT id, title, description, release_date, poster, rating, length, movie_genres.genre_name,
+	(SELECT COUNT(movie_id) FROM user_favorites WHERE user_favorites.movie_id = movies.id) AS favorite_count, 
+	(SELECT COUNT(movie_id) FROM user_watched WHERE user_watched.movie_id = movies.id) AS watched_count 
+	FROM movies	INNER JOIN movie_genres ON id = movie_genres.movie_id;`
 	rows, err := database.DB.Query(sql)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, ErrorMessage(err.Error()))
 		return
 	}
-	fmt.Println(rows)
-	// Return Comments details with the User ids
 	var movies []Movie
 	var movie Movie
+	var genre string
 	for rows.Next() {
-		rows.Scan(&movie.ID, &movie.Title, &movie.ReleaseDate, &movie.Poster, &movie.Rating, &movie.Length)
+		rows.Scan(&movie.ID, &movie.Title, &movie.Description, &movie.ReleaseDate, &movie.Poster, &movie.Rating, &movie.Length, &genre, &movie.FavoriteCount, &movie.WatchedCount)
+		if len(movies) != 0 && movies[len(movies)-1].ID == movie.ID {
+			movies[len(movies)-1].Genres = append(movies[len(movies)-1].Genres, genre)
+			continue
+		}
 		movies = append(movies, movie)
 	}
 	if movies == nil {
-		c.JSON(http.StatusNotFound, ErrorMessage("User not have any movie."))
+		c.JSON(http.StatusNotFound, ErrorMessage("No movie found in the database."))
 		return
 	}
 	c.JSON(http.StatusOK, OkMessage(movies))
