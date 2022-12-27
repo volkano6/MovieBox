2b2d9014537cbc5f674e75833180687ba50f6775
Volkan Öztoklu <volkanoztoklu@gmail.com>
2022-12-27 17:30:29 +0300

    movie update

--- frontend/src/components/single_page/profile_page/user_watched.vue
+++ frontend/src/components/single_page/profile_page/user_watched.vue
@@ -1,32 +1,32 @@
 <template>
-    <div class="row mt-2 row-cols-1 row-cols-xl-5 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-3">
-      <div v-for="movie in watched" :key="movie.id">
-        <MovieCard :poster=movie.poster :title=movie.title :date="movie.release_date" :genre=movie.genres
-          :rating="movie.rating" :length=movie.length :number_of_favorites=movie.favorite_count
-          :number_of_watches=movie.watched_count />
-      </div>
+  <div class="row mt-2 row-cols-1 row-cols-xl-5 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-3">
+    <div v-for="movie in watched" :key="movie.id">
+      <MovieCard :id=movie.id :poster=movie.poster :title=movie.title :date="movie.release_date" :genre=movie.genres
+        :rating="movie.rating" :length=movie.length :number_of_favorites=movie.favorite_count
+        :number_of_watches=movie.watched_count />
     </div>
-  </template>
-  
-  <script>
-  import MovieCard from "../../movie_card.vue"
-  
-  export default {
-    name: "userWatched",
-    props: ["watched"],
-    components: {
-      MovieCard
-    },
-    data() {
-  
-    },
-    created() {
-  
-    }
-  
+  </div>
+</template>
+
+<script>
+import MovieCard from "../../movie_card.vue"
+
+export default {
+  name: "userWatched",
+  props: ["watched"],
+  components: {
+    MovieCard
+  },
+  data() {
+
+  },
+  created() {
+
   }
-  </script>
-  
-  <style>
-  
-  </style>
+
+}
+</script>
+
+<style>
+
+</style>
