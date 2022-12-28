4a4e3b7594d9d877236108bbcd7914a73f1a0905
Volkan Öztoklu <volkanoztoklu@gmail.com>
2022-12-28 23:29:39 +0300

    dashboard is updated

--- frontend/src/views/admin.vue
+++ frontend/src/views/admin.vue
@@ -42,47 +42,55 @@
                 <div class="col-1">
                 </div>
             </div>
-            <div class="row">
-                <div class="col-1 border ">
-                    <p class="mt-2" style="color:white;">1</p>
-                </div>
-                <div class="col-2 border">
-                    <p class="mt-2" style="color:white;">Lord Of the Rings</p>               
-                </div>
-                <div class="col-3 border">
-                    <p class="mt-2" style="color:white;">Dunyanin en iyi filmlerinden</p>
-                </div>
-                <div class="col-2 border">
-                    <p class="mt-2" style="color:white;">1999</p>                
-                </div>
-                <div class="col-1 border">
-                    <!-- <p style="color:white;">Poster</p> -->
-                    <p class="mt-2" style="color:white;">posterling</p>
-                    
-                </div>
-                <div class="col-1 border">
-                    <p class="mt-2" style="color:white;">8.2</p>
-
-                </div>
-                <div class="col-1 border">
-                    <p class="mt-2" style="color:white;">168</p>
-
-                </div>
-                <div class="col-1 border" >
-                    <div style="position:relative; top:5px;">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-pencil-square mt-2" viewBox="0 0 16 16">
-                        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"></path>
-                        <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"></path>
-                        </svg>
+            <div v-for="movie in this.data.movies" :key="movie.id">
+                <div class="row">
+                    <div class="col-1 border ">
+                        <p class="mt-2" style="color:white;">{{ movie.id }}</p>
                     </div>
+                    <div class="col-2 border">
+                        <p class="mt-2" style="color:white;">{{ movie.title }}</p>
+                    </div>
+                    <div class="col-3 border">
+                        <p class="mt-2" style="color:white;">{{ movie.description }}</p>
+                    </div>
+                    <div class="col-2 border">
+                        <p class="mt-2" style="color:white;">{{ movie.release_date }}</p>
+                    </div>
+                    <div class="col-1 border">
+                        <!-- <p style="color:white;">Poster</p> -->
+                        <p class="mt-2" style="color:white;">{{ movie.poster }}</p>
 
-                    <div style="position:relative; left:40px; top:-23px;">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
-                        <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
-                        </svg>
+                    </div>
+                    <div class="col-1 border">
+                        <p class="mt-2" style="color:white;">{{ movie.rating }}</p>
+
+                    </div>
+                    <div class="col-1 border">
+                        <p class="mt-2" style="color:white;">{{ movie.length }}</p>
+
+                    </div>
+                    <div class="col-1 border">
+                        <div style="position:relative; top:5px;">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"
+                                class="bi bi-pencil-square mt-2" viewBox="0 0 16 16">
+                                <path
+                                    d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z">
+                                </path>
+                                <path fill-rule="evenodd"
+                                    d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z">
+                                </path>
+                            </svg>
+                        </div>
+
+                        <div style="position:relative; left:40px; top:-23px;">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"
+                                class="bi bi-trash3" viewBox="0 0 16 16">
+                                <path
+                                    d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z" />
+                            </svg>
+                        </div>
                     </div>
                 </div>
-
             </div>
             <div class="row mt-3">
                 <h3 style="color:white;">Users</h3>
@@ -116,55 +124,63 @@
                 <div class="col-1 border">
                     <h6 class="fw-bold mt-3" style="color:white;">instagram</h6>
                 </div>
-               
+
                 <div class="col-1"></div>
             </div>
+            <div v-for="user in this.data.users" :key="user.id">
             <div class="row">
                 <div class="col-1 border ">
-                    <h6 class="fw-bold mt-3" style="color:white;">1</h6>
+                    <h6 class="fw-bold mt-3" style="color:white;">{{ user.id }}</h6>
                 </div>
                 <div class="col-1 border ">
-                    <h6 class="fw-bold mt-3" style="color:white;">batuhan</h6>
+                    <h6 class="fw-bold mt-3" style="color:white;">{{ user.username }}</h6>
                 </div>
                 <div class="col-2 border">
-                    <h6 class="fw-bold mt-3" style="color:white;">bulu</h6>
+                    <h6 class="fw-bold mt-3" style="color:white;">{{ user.displayname }}</h6>
                 </div>
                 <div class="col-2 border">
-                    <h6 class="fw-bold mt-3" style="color:white;">bulu@gmail.com</h6>
+                    <h6 class="fw-bold mt-3" style="color:white;">{{ user.email }}</h6>
                 </div>
                 <div class="col-1 border">
-                    <h6 class="fw-bold mt-3" style="color:white;">03.04.2000</h6>
+                    <h6 class="fw-bold mt-3" style="color:white;">{{ user.date_of_birth }}</h6>
                 </div>
                 <div class="col-1 border">
                     <!-- <p style="color:white;">Poster</p> -->
-                    <h6 class="fw-bold mt-3" style="color:white;">hello</h6>
+                    <h6 class="fw-bold mt-3" style="color:white;">{{ user.bio }}</h6>
                 </div>
                 <div class="col-1 border">
-                    <h6 class="fw-bold mt-3" style="color:white;">istanbul</h6>
+                    <h6 class="fw-bold mt-3" style="color:white;">{{ user.location }}</h6>
                 </div>
                 <div class="col-1 border">
-                    <h6 class="fw-bold mt-3" style="color:white;">bulu</h6>
+                    <h6 class="fw-bold mt-3" style="color:white;">{{ user.twitter }}</h6>
                 </div>
                 <div class="col-1 border">
-                    <h6 class="fw-bold mt-3" style="color:white;">bulu</h6>
+                    <h6 class="fw-bold mt-3" style="color:white;">{{ user.instagram }}</h6>
                 </div>
-            
+
                 <div class="col-1 border">
                     <div style="position:relative; top:5px;">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-pencil-square mt-2" viewBox="0 0 16 16">
-                        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"></path>
-                        <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"></path>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"
+                            class="bi bi-pencil-square mt-2" viewBox="0 0 16 16">
+                            <path
+                                d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z">
+                            </path>
+                            <path fill-rule="evenodd"
+                                d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z">
+                            </path>
                         </svg>
                     </div>
 
                     <div style="position:relative; left:40px; top:-23px;">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
-                        <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"
+                            class="bi bi-trash3" viewBox="0 0 16 16">
+                            <path
+                                d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z" />
                         </svg>
                     </div>
                 </div>
             </div>
-
+        </div>
         </div>
 
         <Footer></Footer>
@@ -173,22 +189,35 @@
 </template>
 
 <script>
+import axios from 'axios'
 import Nav from "../components/nav.vue";
 import Footer from "../components/footer.vue";
 
 export default {
     name: "admin",
+    data() {
+        return {
+            data: []
+        }
+    },
     components: {
         Nav,
         Footer,
     },
+    async created() {
+        const response = await axios.get("api/dashboard");
+        if (response.data.status == "error") {
+            this.$router.push("/home")
+        } else {
+            this.data = response.data
+        }
+        console.log(this.data)
+    }
 }
 </script>
 
 <style>
 svg {
-  fill: white;
+    fill: white;
 }
-
-
 </style>
--- frontend/src/views/home.vue
+++ frontend/src/views/home.vue
@@ -84,6 +84,7 @@ export default {
     } else {
       this.movies = response.data
     }
+    
   }
 }
 </script>
